af3e55167654feb5fd4e57abd2296a25be1fc835
nopyjf <rockon23146@gmail.com>
2023-07-08 22:13:31 +0700

    Start project

---
+++ android/app/src/main/kotlin/com/nopyjf/codegen/nopyjf_code_gen/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.nopyjf.codegen.nopyjf_code_gen
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/modules/selection/ui/SelectionListScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SelectionListScreen extends StatefulWidget {
+  const SelectionListScreen({super.key});
+
+  @override
+  State<SelectionListScreen> createState() => _SelectionListScreenState();
+}
+
+class _SelectionListScreenState extends State<SelectionListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Hello");
+  }
+}
