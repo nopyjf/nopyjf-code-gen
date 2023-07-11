22b902044d0c8caeacf9676ee0bdc3acace744f6
NITHICHAI THEPMONG <nithichai.thepmong@scbtechx.io>
2023-07-11 10:48:06 +0700

    feat: add some file for selection bloc

---
+++ lib/modules/selection/bloc/selection_event.dart
@@ -0,0 +1,8 @@
+part of 'selection_bloc.dart';
+
+class SelectionEvent extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
+
+class SelectionComponentEvent extends SelectionEvent {}
